316568d3a18a72bfa32a994cebc348992beed1b2
Chad Messimer <103394173+cmessimer@users.noreply.github.com>
2024-05-07 06:03:04 -0400

    C Sharp Practice

---
+++ FirstProgramCDM/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using FirstProgramCDM; //allows use of Utilities Class
+
+// int amount = 1234;
+// int months = 12;
+// int bonus = 1000;
+
+// int yearlyWage = Utilities.CalculateYearlyWage(amount, months); 
+
+// Console.WriteLine($"Yearly wage: {yearlyWage}");
+
+// double amountDouble = 1500.0;
+// double monthsDouble = 12;
+// double bonusDouble = 1000;
+
+// double yearlyWageWithBonusDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
+
+
+// Comment everything above this point in order to use optional parameters(OP1)
+// Utilities.UsingOpitonalParameters();
+
+// Comment everything above this point in order to use optional parameters(OP2)
+// Utilities.UsingNamedArguments();
+
+//  Comment everything above this point in order to use optional parameters(OP3)
+Utilities.UsingOptionalParameters();
+
+Console.ReadLine();
